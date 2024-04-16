c4713e9a09eb9ba24de240a3ea7f69bccac61b2b
Igor Terriaga <igsantos1996@gmail.com>
2024-04-15 21:50:29 -0300

    start vitest

---
+++ src/entities/appointment.spec.ts
@@ -0,0 +1,29 @@
+import { expect, test } from "vitest";
+import { Appointment } from "./appointment";
+
+test("create and Appointment", () => {
+  const startAt = new Date();
+  const endsAt = new Date();
+  endsAt.setDate(endsAt.getDate() + 1);
+  const appointment = new Appointment({
+    customer: "John Doe",
+    startAt,
+    endsAt,
+  });
+  expect(appointment).toBeInstanceOf(Appointment);
+  expect(appointment.customer).equal("John Doe");
+});
+
+test("cannot create and appointment with end date before start date", () => {
+  const startAt = new Date();
+  const endsAt = new Date();
+
+  endsAt.setDate(endsAt.getDate() - 1);
+  expect(() => {
+    return new Appointment({
+      customer: "John Doe",
+      startAt,
+      endsAt,
+    });
+  }).toThrow();
+});
---
+++ src/entities/appointment.ts
@@ -0,0 +1,28 @@
+export interface IAppointment {
+  customer: string;
+  startAt: Date;
+  endsAt: Date;
+}
+export class Appointment {
+  private props: IAppointment;
+
+  get customer(): string {
+    return this.props.customer;
+  }
+
+  get startAt(): Date {
+    return this.props.startAt;
+  }
+
+  get endsAt(): Date {
+    return this.props.endsAt;
+  }
+
+  constructor(appointment: IAppointment) {
+    const { startAt, endsAt } = appointment;
+    if (endsAt <= startAt) {
+      throw new Error("Invalid end Date");
+    }
+    this.props = appointment;
+  }
+}
